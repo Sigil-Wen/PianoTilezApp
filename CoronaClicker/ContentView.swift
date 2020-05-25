cfaefb899b4223cac2cfe429765e4b7f95dc4839
Sigil-Wen <sigil.w3n@gmail.com>
2020-05-25 14:08:03 -0400

    Initial Commit

---
+++ CoronaClicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaClicker
+//
+//  Created by sigil wen on 2020-05-25.
+//  Copyright © 2020 Sigil Wen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
